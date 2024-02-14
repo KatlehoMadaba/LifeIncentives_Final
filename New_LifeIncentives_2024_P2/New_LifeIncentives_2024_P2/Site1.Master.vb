2fa9b04ebad8819fb89d7a545f9dbd7d13d6b6f0
deggaknight <76885877+deggaknight@users.noreply.github.com>
2024-02-14 09:50:02 +0200

    copy and paste from previous repository

---
+++ New_LifeIncentives_2024_P2/New_LifeIncentives_2024_P2/Site1.Master.vb
@@ -0,0 +1,10 @@
+﻿Public Class Site1
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If IsNothing(HttpContext.Current.Session("conn")) Then
+            HttpContext.Current.Session("conn") = DBInterface.connect(ConfigurationManager.ConnectionStrings("conn2").ConnectionString)
+        End If
+    End Sub
+
+End Class
---
+++ New_LifeIncentives_2024_P2/New_LifeIncentives_2024_P2/signUp.aspx.vb
@@ -0,0 +1,39 @@
+﻿
+Public Class signUp
+    Inherits System.Web.UI.Page
+
+    Dim pu As New TBL_Parent_User
+    Dim cu As New TBL_Child_User
+    Dim fi As New TBL_Financial_Information
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If IsNothing(HttpContext.Current.Session("conn")) Then
+            HttpContext.Current.Session("conn") = DBInterface.connect(ConfigurationManager.ConnectionStrings("conn2").ConnectionString)
+        End If
+    End Sub
+
+    Protected Sub Register(sender As Object, e As EventArgs)
+
+
+
+        cu.Child_ID = ChildID.Text
+        cu.Child_Name = ChildName.Text
+        cu.Child_Surname = ChildSurname.Text
+        cu.Child_Age = Integer.Parse(ChildAge.Text)
+        cu.Child_Password = ChildPassword.Text
+
+        Session("childID") = cu.Child_ID
+
+        cu.update()
+
+        Response.Redirect("signUpParent.aspx")
+
+    End Sub
+
+
+
+
+
+
+End Class
