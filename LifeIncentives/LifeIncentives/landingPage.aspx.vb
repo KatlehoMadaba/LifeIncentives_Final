2a57eae78582d1c0d12a3946f48db36fa9ef6bbf
ibrahim2k03 <ibrahimkabore1108@gmail.com>
2024-02-26 09:10:33 +0200

    FIXED

--- LifeIncentives/LifeIncentives/landingPage.aspx.vb
+++ LifeIncentives/LifeIncentives/landingPage.aspx.vb
@@ -6,18 +6,11 @@
     End Sub
 
     Protected Sub btnLogin_Click(ByVal sender As Object, ByVal e As EventArgs)
-        ' Redirect to the parentLogin.aspx page or add your login logic here
 
-        ' Get the selected value of the dropdown
-        Dim selectedValue As String = DropDownList.SelectedValue
-
-        ' Redirect to the corresponding page based on the value
-        Response.Redirect(selectedValue & ".aspx")
 
 
     End Sub
-    Protected Sub DropDownList1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles DropDownList.SelectedIndexChanged
-        Response.Redirect(DropDownList.SelectedItem.Value)
+    Protected Sub DropDownList1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs)
     End Sub
 
 End Class
