80f6bbe6c7d8007d671a6cd6b49b8b9e594f3901
ibrahim2k03 <ibrahimkabore1108@gmail.com>
2024-02-28 22:12:58 +0200

    ...

--- LifeIncentives/LifeIncentives/childDashboard.aspx.vb
+++ LifeIncentives/LifeIncentives/childDashboard.aspx.vb
@@ -1,8 +1,73 @@
-﻿Public Class childDashboard
+﻿Imports System.Data.SqlClient
+
+Public Class childDashboard
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+    Protected Sub btnViewRequests_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnSendRequest_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnIncParent_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnDecParent_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnIncGov_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnDecGov_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub BTNViewrequest_Click(sender As Object, e As ImageClickEventArgs)
+
+    End Sub
+
+    Protected Sub btnsendPInc_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsendPDec_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsendGInc_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsendGdec_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsubPInc_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsubPDec_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsubGInc_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsubGDec_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnsubPDec_Click1(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
